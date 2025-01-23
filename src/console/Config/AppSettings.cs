cd56db769dded0e65cd67961122b268810776ea1
Matt Salmon <matt@iua.co.za>
2025-01-23 17:06:45 +0200

    Added app settings and app config files to console project

---
+++ src/console/Config/AppSettings.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mistral.Config
+{
+    internal class AppSettings
+    {
+        private readonly IConfigurationSection _appSettings;
+        private readonly IConfigurationRoot _appSecrets;
+
+        private AppSettings(IConfigurationSection? appSettings, IConfigurationRoot? appSecrets)
+        {
+            if (appSettings == null) 
+            {
+                throw new InvalidOperationException("AppSettings file not loaded");
+            }
+            if (appSecrets == null)
+            {
+                throw new InvalidOperationException("AppSecrets file not loaded");
+            }
+            this._appSettings = appSettings;
+            this._appSecrets = appSecrets;
+        }
+
+        public string TestKey  => this._appSettings["TestKey"]!;
+
+        public string MistralApiKey => this._appSecrets["MistralApiKey"]!;
+
+        public static AppSettings Load()
+        {
+            string basePath = AppDomain.CurrentDomain.BaseDirectory;
+
+            var appConfig = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("AppSettings.json")
+                .Build()
+                .GetRequiredSection("AppSettings");
+
+            var appSecrets = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("AppSecrets.json")
+                .Build();
+
+            return  new AppSettings(appConfig, appSecrets);
+        }
+    }
+}
--- src/console/Program.cs
+++ src/console/Program.cs
@@ -1,4 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using Mistral.Config;
+
+AppSettings appSettings = AppSettings.Load();
+
+// See https://aka.ms/new-console-template for more information
 Console.BackgroundColor = ConsoleColor.Green;
 Console.WriteLine("Hello, World!");
 Console.BackgroundColor = ConsoleColor.Black;
