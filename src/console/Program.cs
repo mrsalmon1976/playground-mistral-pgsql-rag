dc90a3169b95d1e76ba0b09ffbbc356656354369
Matt Salmon <matt@iua.co.za>
2025-01-23 17:18:49 +0200

    Added MistralClient and submitted a basic question.

--- src/console/Program.cs
+++ src/console/Program.cs
@@ -1,9 +1,33 @@
 ﻿
 using Mistral.Config;
+using Mistral.SDK;
+using Mistral.SDK.DTOs;
 
 AppSettings appSettings = AppSettings.Load();
 
-// See https://aka.ms/new-console-template for more information
-Console.BackgroundColor = ConsoleColor.Green;
-Console.WriteLine("Hello, World!");
-Console.BackgroundColor = ConsoleColor.Black;
+var client = new MistralClient(appSettings.MistralApiKey);
+var request = new ChatCompletionRequest(
+    //define model - required
+    ModelDefinitions.MistralMedium,
+    //define messages - required
+    new List<ChatMessage>()
+{
+    new ChatMessage(ChatMessage.RoleEnum.System,
+        "You are profession claims assessor."),
+    new ChatMessage(ChatMessage.RoleEnum.User,
+        "Write me a short introduction on how to assess a claim.")
+},
+    //optional - defaults to false
+    safePrompt: true,
+    //optional - defaults to 0.7
+    temperature: 0.7M,
+    //optional - defaults to null
+    maxTokens: 500,
+    //optional - defaults to 1
+    topP: 1,
+    //optional - defaults to null
+    randomSeed: 32);
+var response = await client.Completions.GetCompletionAsync(request);
+Console.WriteLine(response.Choices.First().Message.Content);
+
+
